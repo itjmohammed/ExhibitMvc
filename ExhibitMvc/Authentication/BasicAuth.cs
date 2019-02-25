81784e8b8edcac93958496bf0188e1bdd510b610
hojm <hojm85@gmail.com>
2019-02-25 19:03:08 +0000

    up to the time!

---
+++ ExhibitMvc/Authentication/BasicAuth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nexmo.Api;
+
+namespace ExhibitMvc.Authentication
+{
+  public class BasicAuth
+  {
+    public BasicAuth()
+    {
+      var client = new Client(creds: new Nexmo.Api.Request.Credentials
+      {
+        ApiKey = "NEXMO_API_KEY",
+        ApiSecret = "NEXMO_API_SECRET"
+      });
+    }
+  }
+}
